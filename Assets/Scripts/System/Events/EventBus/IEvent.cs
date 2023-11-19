397c28ce86ae51c7a1eeb18420d9913c3145ef2e
gDavASko <DavASko01@gmail.com>
2023-11-20 02:13:00 +0500

    - add Eventt Bus

---
+++ Assets/Scripts/System/Events/EventBus/IEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace RFW.Events
+{
+    public interface IEvent
+    {
+    }
+}
---
+++ Assets/Scripts/System/Events/EventBus/IEventBinding.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RFW.Events
+{
+    public interface IEventBinding<T>
+    {
+        public Action<T> OnEvent { get; set; }
+        public Action OnEventNoArgs { get; set; }
+    }
+}
